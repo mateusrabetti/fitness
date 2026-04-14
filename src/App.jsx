75eeba66acbb0d20d0987ac64c5b9747b4126e8b
mateusrabetti <31521@raphaeldisanto.com.br>
2026-04-14 08:59:02 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <main> 
+        <header>
+          <div className='foto-perfil'>
+            <img src="" alt="" />
+          </div>
+
+          <div>
+            <h2>Bem-vindo</h2>
+            <p>Mateus, Brasil</p>
+          </div>
+
+          <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+        </header>
+      </main>  
+    </>
+  )
+}
+
