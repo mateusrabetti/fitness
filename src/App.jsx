ee9321c9d53c0239b235b81cc8c330afc63822d5
mateusrabetti <31521@raphaeldisanto.com.br>
2026-04-14 11:38:35 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,57 @@ export default function App() {
             <img src="" alt="" />
           </div>
         </header>
+
+        {/* ------------------------------------------------------------------------------------  */}
+
+        <section className='info'>
+          <div>
+            <h1> Atividades de Hoje</h1>
+            <h2>Peso Corporal</h2>
+          </div>
+
+          <div className='%'>
+
+          </div>
+
+          <div>
+            <h3>1200 kcal</h3>
+            <h3>90 bpm</h3>
+            <h3>03:00</h3>
+          </div>
+
+          <div>
+            <p>Calorias Queimadas</p>
+            <p>Frequência Cardíaca</p>
+            <p>Tempo</p>
+          </div>
+
+        </section>
+
+
+        <section className='categorias'>
+          <div>
+            <h2></h2>
+            <a href="">Mostrar tudo</a>
+          </div>
+
+          <div>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+          </div>
+        </section>
+
+
+
+        <section className='celebridades'>
+          
+        </section>
+
+        <footer>
+          
+        </footer>
       </main>  
     </>
   )
